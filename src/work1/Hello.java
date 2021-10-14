35fd52e2a16c7bdfc53b40aa5e1c622bd4d86f0d
mac <gulnazbakha@gmail.com>
2021-10-14 00:47:28 -0500

    Added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("Created Git Repository");
         System.out.println("Added");
+        System.out.println("Made few commits");
     }
 }
